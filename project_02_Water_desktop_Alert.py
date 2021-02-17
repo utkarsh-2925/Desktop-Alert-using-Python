98a509d72d5927c9ec333b31fee19a8ae7bdd096
UTKARSH KUSHWAHA <71309361+utkarsh-2925@users.noreply.github.com>
2021-02-17 18:22:50 +0530

    Add files via upload

---
+++ project_02_Water_desktop_Alert.py
@@ -0,0 +1,11 @@
+import time
+
+from plyer import notification
+
+if __name__ == '__main__':
+  notification.notify(
+    title = "Time to drink Water 'NOW'!!.⏰",
+    message = "DrinkingWater, Increases Energy & Relieves Fatigue. ",
+    timeout = 10
+  )
+      
